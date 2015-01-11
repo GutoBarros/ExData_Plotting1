af8f80abd02ed4097e15f188883b9a8ebcf9f4bf
GutoBarros <joseaugustobarros@gmail.com>
2015-01-11 11:00:39 -0300

    Update to current class
    I did this assignment before, but not concluded the class. So I updated
    it to match the current one.

--- plot1.R
+++ plot1.R
@@ -1,25 +1,26 @@
-## first plot script, project 1 on Exploratory Data Analysis
+## First plot script, project 1 on Exploratory Data Analysis
+# Creates the dataset if it doesn't exists
 
-# Creates the dataset, if it doesn't exists
-
-if (!file.exists("./subspower.txt")){
-      power <- read.csv("./household_power_consumption.txt", sep=";", 
-                        stringsAsFactors=FALSE, colClasses=c(rep("character",2),
-                                                             rep("numeric",7)), na.strings="?")
-      datahora <- paste(power$Date,power$Time)
-      power <- cbind(power,datahora)
-      power$datahora <- strptime(power$datahora, format="%d/%m/%Y %H:%M:%S")
-      subspower<-subset(power,(Date=="1/2/2007" | Date=="2/2/2007"))
-      write.table(subspower, file="subspower.txt", sep="\t")
+if (!file.exists("./net_data.txt")){
+      gross_data <- read.csv("./household_power_consumption.txt", sep=";", 
+                             na.strings="?",stringsAsFactors=FALSE)
+      datahora <- paste(gross_data$Date,gross_data$Time)
+      gross_data <- cbind(gross_data,datahora)
+      net_data <- gross_data[(gross_data$Date=="1/2/2007" 
+                              | gross_data$Date=="2/2/2007"),]
+      net_data$datahora <- strptime(net_data$datahora,
+                                    format="%d/%m/%Y %H:%M:%S")
+      write.table(net_data, file="net_data.txt", sep="\t")
 }else{
-      subspower <- read.table("./subspower.txt", sep="\t", stringsAsFactors=FALSE)
-      subspower$datahora <- strptime(subspower$datahora, format="%Y-%m-%d %H:%M:%S")     
+      net_data <- read.csv("./net_data.txt", sep="\t", stringsAsFactors=FALSE)
+      net_data$datahora <- strptime(net_data$datahora,
+                                    format="%Y-%m-%d %H:%M:%S")
 }
 
-#makes the plot 
+# Makes the plot
 
 Sys.setlocale("LC_TIME", "English")
-png("plot1.png",width=480,height=480,units="px")
-hist(subspower$Global_active_power, main="Global Active Power", breaks=12,
-     col="red",xlab="Global Active Power (kilowatts)")
+png(filename="plot1.png", width = 480, height = 480, units="px")
+hist(net_data$Global_active_power, main="Global Active Power", breaks=12,
+     col="red", xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,40 +1,42 @@
-## fourth plot script, project 1 on Exploratory Data Analysis
+## Fourth plot script, project 1 on Exploratory Data Analysis
+# Creates the dataset if it doesn't exists
 
-# Creates the dataset, if it doesn't exists
-
-if (!file.exists("./subspower.txt")){
-      power <- read.csv("./household_power_consumption.txt", sep=";", 
-                        stringsAsFactors=FALSE, colClasses=c(rep("character",2),
-                                                             rep("numeric",7)), na.strings="?")
-      datahora <- paste(power$Date,power$Time)
-      power <- cbind(power,datahora)
-      power$datahora <- strptime(power$datahora, format="%d/%m/%Y %H:%M:%S")
-      subspower<-subset(power,(Date=="1/2/2007" | Date=="2/2/2007"))
-      write.table(subspower, file="subspower.txt", sep="\t")
+if (!file.exists("./net_data.txt")){
+      gross_data <- read.csv("./household_power_consumption.txt", sep=";", 
+                             na.strings="?",stringsAsFactors=FALSE)
+      datahora <- paste(gross_data$Date,gross_data$Time)
+      gross_data <- cbind(gross_data,datahora)
+      net_data <- gross_data[(gross_data$Date=="1/2/2007" 
+                              | gross_data$Date=="2/2/2007"),]
+      net_data$datahora <- strptime(net_data$datahora,
+                                    format="%d/%m/%Y %H:%M:%S")
+      write.table(net_data, file="net_data.txt", sep="\t")
 }else{
-      subspower <- read.table("./subspower.txt", sep="\t", stringsAsFactors=FALSE)
-      subspower$datahora <- strptime(subspower$datahora, format="%Y-%m-%d %H:%M:%S")     
+      net_data <- read.csv("./net_data.txt", sep="\t", stringsAsFactors=FALSE)
+      net_data$datahora <- strptime(net_data$datahora,
+                                    format="%Y-%m-%d %H:%M:%S")
 }
 
-#makes the plot 
+# Makes the plot
 
 Sys.setlocale("LC_TIME", "English")
-png("plot4.png",width=480,height=480,units="px")
-par(mfrow = c(2,2))
-with(subspower,{ plot(datahora, Global_active_power, type ="n",
-                      ylab="Global Active Power", xlab="")
-                 lines(datahora, Global_active_power)
-                 plot(datahora, Voltage, type ="n",
-                      ylab="Voltage", xlab="datetime")
-                 lines(datahora,Voltage)
-                 plot(datahora, Sub_metering_1, type= "n", ylab="Energy sub metering",xlab="")
-                 lines(datahora, Sub_metering_1, col = "black")
-                 lines(datahora, Sub_metering_2, col = "red")
-                 lines(datahora, Sub_metering_3, col = "blue")
-                 legend("topright", lwd=2, col=c("black" ,"red", "blue"),
-                        legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-                 plot(datahora, Global_reactive_power, type="n", xlab="datetime",
-                      ylab="Global_reactive_power")
-                 lines(datahora, Global_reactive_power)
-})
+png(filename="plot4.png", width = 480, height = 480, units = "px")
+par(mfrow=c(2,2))
+with (net_data, {plot(datahora, Global_active_power, type = "n",
+					ylab="Global Active Power (kilowatts)", xlab="")
+				lines(datahora, Global_active_power)
+				plot(datahora, Voltage, type = "n",
+					ylab="Voltage", xlab="datetime")
+				lines(datahora, Voltage)
+				plot(datahora, Sub_metering_1, type = "n",
+					ylab="Energy sub metering", xlab="")
+				lines(datahora, Sub_metering_1, col="black")
+				lines(datahora, Sub_metering_2, col="red")
+				lines(datahora, Sub_metering_3, col="blue")
+				legend("topright", bty="n", lwd=2, col=c("black" ,"red", "blue"),
+					legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+				plot(datahora, Global_reactive_power, type = "n",
+					ylab="Global_reactive_power", xlab="datetime")
+				lines(datahora, Global_reactive_power)
+				})
 dev.off()
